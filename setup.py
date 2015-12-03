335433be2b14f8f43e43744aabc8262cf3c36924
Harshad Sharma <harshad@sharma.io>
2015-12-03 12:55:31 +0530

    Causing missing package warning, not needed since there's but one file to package.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='uconfig',
     version='0.1',
-    packages=['uconfig'],
     include_package_data=True,
     install_requires=[
         'pyyaml',
